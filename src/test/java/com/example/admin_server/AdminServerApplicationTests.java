103dc09c5492ed4149c64ac206f00288aa509fa1
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-10-10 09:12:36 +0300

    init commit

---
+++ src/test/java/com/example/admin_server/AdminServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.admin_server;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminServerApplicationTests {
+
+    @Test
+    @Ignore
+    public void contextLoads() {
+    }
+
+}
